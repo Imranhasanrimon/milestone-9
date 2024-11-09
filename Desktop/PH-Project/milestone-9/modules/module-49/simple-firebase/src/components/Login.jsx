5c17f9c139c1fafd168d6889e31dfac8c88a0390
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-09 06:46:42 +0600

    signin popup by firebase auth

--- Desktop/PH-Project/milestone-9/modules/module-49/simple-firebase/src/components/Login.jsx
+++ Desktop/PH-Project/milestone-9/modules/module-49/simple-firebase/src/components/Login.jsx
@@ -1,8 +1,20 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../firebase/firebase.init";
 
 const Login = () => {
+    const handleGoogleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
     return (
         <div>
-            <h1>login with google</h1>
+            <button onClick={handleGoogleSignIn}>login with google</button>
         </div>
     );
 };
