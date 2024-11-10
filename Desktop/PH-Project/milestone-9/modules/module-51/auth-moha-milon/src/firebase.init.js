ee82586561e6895c4d48e1c2b4f2732aff87ee7c
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-10 23:58:21 +0600

    creating authprovider comop

---
+++ Desktop/PH-Project/milestone-9/modules/module-51/auth-moha-milon/src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDAPNMfdrfbtFlkXc3KxMzjr0eO9-UDqyA",
+    authDomain: "auth-moha-milon-c9aec.firebaseapp.com",
+    projectId: "auth-moha-milon-c9aec",
+    storageBucket: "auth-moha-milon-c9aec.firebasestorage.app",
+    messagingSenderId: "983243240203",
+    appId: "1:983243240203:web:cab1d82ecf9a7f05b7e80e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
