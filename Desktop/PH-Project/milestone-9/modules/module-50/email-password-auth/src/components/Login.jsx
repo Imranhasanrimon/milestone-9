2004d25190b21ea7a21d3935aaa6a4242130addb
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-10 07:36:19 +0600

    navbar is done

---
+++ Desktop/PH-Project/milestone-9/modules/module-50/email-password-auth/src/components/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div>
+            <h3 className="text-4xl">Login</h3>
+        </div>
+    );
+};
+
+export default Login;
---
+++ Desktop/PH-Project/milestone-9/modules/module-50/email-password-auth/src/components/Register.jsx
@@ -0,0 +1,10 @@
+
+const Register = () => {
+    return (
+        <div>
+            <h3 className="text-4xl">Register</h3>
+        </div>
+    );
+};
+
+export default Register;
