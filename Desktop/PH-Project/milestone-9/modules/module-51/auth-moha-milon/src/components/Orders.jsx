f0442cbf6e3f05f356f7be76365cb94a58f5e5d9
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-11 18:51:01 +0600

    creating orders component

---
+++ Desktop/PH-Project/milestone-9/modules/module-51/auth-moha-milon/src/components/Orders.jsx
@@ -0,0 +1,14 @@
+
+const Orders = () => {
+    return (
+        <div>
+            <h1>item-1</h1>
+            <h1>item-2</h1>
+            <h1>item-3</h1>
+            <h1>item-4</h1>
+            <h1>item-5</h1>
+        </div>
+    );
+};
+
+export default Orders;
--- Desktop/PH-Project/milestone-9/modules/module-51/auth-moha-milon/src/main.jsx
+++ Desktop/PH-Project/milestone-9/modules/module-51/auth-moha-milon/src/main.jsx
@@ -10,6 +10,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import AuthProvider from './providers/AuthProvider';
+import Orders from './components/Orders';
 
 const router = createBrowserRouter(
   [
@@ -28,6 +29,10 @@ const router = createBrowserRouter(
         {
           path: "/register",
           element: <Register></Register>
+        },
+        {
+          path: "/orders",
+          element: <Orders></Orders>
         }
       ]
     },
