4561a521a26d5654192f19e35d54ce84ce73a118
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-13 22:26:56 +0600

    importing createBrowserRouter

--- Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/routes/router.jsx
+++ Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/routes/router.jsx
@@ -1,4 +1,4 @@
-const { createBrowserRouter } = require("react-router-dom");
+import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +13,19 @@ const router = createBrowserRouter([
         path: "/auth",
         element: <h1>this is Login page</h1>
     },
-])
+    {
+        path: "*",
+        element: <h1>Error</h1>
+    },
+], {
+    future: {
+        v7_startTransition: true,
+        v7_skipActionErrorRevalidation: true,
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_relativeSplatPath: true,
+    },
+})
 
 export default router;
