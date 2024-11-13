f4a7eeca166750c253cbc0c9f8af4685789dc8fe
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-13 23:13:33 +0600

    installing Marquee

--- Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/components/LatestNews.jsx
+++ Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/components/LatestNews.jsx
@@ -1,9 +1,16 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 const LatestNews = () => {
     return (
         <div className="flex items-center gap-2">
             <p className="bg-red-400 text-base-100 py-1 px-3">Latest</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis illo qui facilis nulla aliai, rerum quas.</p>
+            <Marquee className="bg-gray-200 py-1 space-x-10"
+                pauseOnHover={true}>
+                <Link to="/"> I can be a React component</Link>
+                <Link to="/news"> Lorem ipsum dolor sit amet consectetur adipisicing .</Link>
+                <Link to="/about"> sit amet consectetur adipisicing</Link>
+            </Marquee>
         </div>
     );
 };
