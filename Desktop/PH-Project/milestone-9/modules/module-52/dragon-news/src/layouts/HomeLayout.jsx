05a44ed7eb7cc43a15975e4e1db76a57c4ee8946
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-14 15:33:21 +0600

    loading data based on category

--- Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/layouts/HomeLayout.jsx
+++ Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/layouts/HomeLayout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import LeftNavbar from "../components/LeftNavbar";
@@ -18,7 +19,7 @@ const HomeLayout = () => {
             </nav>
             <main className="grid md:grid-cols-12 gap-5 mt-10">
                 <aside className="col-span-3"><LeftNavbar></LeftNavbar></aside>
-                <section className="col-span-6">main conter</section>
+                <section className="col-span-6"><Outlet></Outlet></section>
                 <aside className="col-span-3"><RightNavbar></RightNavbar></aside>
             </main>
         </div>
