c3598795096c35456de98af9620bb7ce6119d463
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-14 15:41:46 +0600

    creating newsCard component

--- Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/assets/pages/CategoryNews.jsx
+++ Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/assets/pages/CategoryNews.jsx
@@ -1,11 +1,17 @@
 import { useLoaderData } from "react-router-dom";
+import NewsCard from "../../components/NewsCard";
 
 const CategoryNews = () => {
-    const { data } = useLoaderData();
-    console.log(data);
+    const { data: news } = useLoaderData();
     return (
         <div>
-            {data.length} news found
+            <h3 className="font-semibold mb-3">Dragon News</h3>
+            <p className="text-gray-500 font-semibold">{news.length} news found</p>
+            <div>
+                {
+                    news.map(singleNews => <NewsCard key={singleNews.id} singleNews={singleNews}></NewsCard>)
+                }
+            </div>
         </div>
     );
 };
---
+++ Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/components/NewsCard.jsx
@@ -0,0 +1,11 @@
+
+const NewsCard = ({ singleNews }) => {
+    return (
+        <div>
+            <p>{singleNews.title}</p>
+
+        </div>
+    );
+};
+
+export default NewsCard;
