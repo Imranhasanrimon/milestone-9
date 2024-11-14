8d8c8083658f6097c18dc14afd28186e7ef80f18
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-14 08:46:58 +0600

    creating leftNavbar components

---
+++ Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/components/LeftNavbar.jsx
@@ -0,0 +1,11 @@
+
+const LeftNavbar = () => {
+    return (
+        <div>
+            <h3>All Categories (8)</h3>
+
+        </div>
+    );
+};
+
+export default LeftNavbar;
--- Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/layouts/HomeLayout.jsx
+++ Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/LeftNavbar";
 import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
@@ -14,7 +15,11 @@ const HomeLayout = () => {
             <nav>
                 <Navbar></Navbar>
             </nav>
-            <main></main>
+            <main className="grid md:grid-cols-12">
+                <aside className="col-span-3"><LeftNavbar></LeftNavbar></aside>
+                <section className="col-span-6">main conter</section>
+                <aside className="col-span-3">Right Nvbar</aside>
+            </main>
         </div>
     );
 };
