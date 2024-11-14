48430cb89cc882295c24f39d1df0718876faebfd
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-14 08:52:58 +0600

    fetching data for categories

--- Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/components/LeftNavbar.jsx
+++ Desktop/PH-Project/milestone-9/modules/module-52/dragon-news/src/components/LeftNavbar.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 
 const LeftNavbar = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch(' https://openapi.programming-hero.com/api/news/categories')
+            .then(response => response.json())
+            .then(data => setCategories(data.data.news_category))
+    }, [])
     return (
         <div>
             <h3>All Categories (8)</h3>
-
+            <div>
+                {
+                    categories.map(category => <button key={category.category_id}>{category.category_name}</button>)
+                }
+            </div>
         </div>
     );
 };
